20a75785a0d0ed0d0ee84383cda676ada8839140
smsag99@gmail.com <smsag@gmail.com>
2023-03-10 13:34:39 +0330

    adding routes files

---
+++ src/routes/v1/forgetPassword.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  console.log("teeeeeee");
+  res.send("ok");
+});
+router.post("/", (req, res) => {
+  console.log("teeeeeee");
+  res.send("ok");
+});
+
+module.exports = router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const authV1 = require("./auth.js");
+const forgetPasswordV1 = require("./forgetPassword.routes.js");
 
 const v1Loader = () => {
   const router = express.Router();
   router.use("/auth", authV1);
+  router.use("/forgetPassword", forgetPasswordV1);
   return router;
 };
 module.exports = v1Loader;
