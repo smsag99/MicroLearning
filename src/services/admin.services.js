b721a2276d5d4a6d7e5327452e1e2f4cf493543c
smsag99@gmail.com <smsag@gmail.com>
2023-03-11 19:22:40 +0330

    auth admin is done

--- src/services/admin.services.js
+++ src/services/admin.services.js
@@ -6,18 +6,83 @@ require("dotenv").config();
 
 const prisma = new PrismaClient();
 
-const signup = async (req) => {};
+const signup = async (req) => {
+  const { userName, password } = req.body;
+  const admin = await getAdminbyUserName(userName);
+  if (admin) {
+    return "This admin Already Exists!";
+  } else {
+    await createAdmin(userName, password);
+  }
+};
 
-function getAdminbyId(objectId) {}
+async function getAdminbyId(objectId) {
+  return await prisma.Admin.findUnique({
+    where: {
+      id: objectId,
+    },
+  });
+}
 
-async function getAdminbyUserName(userName) {}
+async function getAdminbyUserName(userName) {
+  console.log(userName);
+  return await prisma.Admin.findUnique({
+    where: {
+      userName: userName,
+    },
+  });
+}
 
-async function setRefereshToken(userName) {}
+async function setRefereshToken(userName) {
+  const admin = await getAdminbyUserName(userName);
+  console.log(admin);
+  const refreshToken = await jwt.sign(
+    { objectId: admin.objectId, userName: userName },
+    "12345678",
+    { expiresIn: 3600000 * 1000 }
+  );
+  const accessToken = await jwt.sign(
+    { objectId: admin.objectId, userName: userName },
+    "12345678",
+    { expiresIn: 3600000 }
+  );
+  if (await updateAdmin({ userName, refreshToken })) {
+    return accessToken;
+  }
+}
 
-async function createAdmin(userName, password) {}
+async function createAdmin(userName, password) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const admin = await prisma.Admin.create({
+    data: {
+      userName: userName,
+      password: hashedPassword,
+    },
+  });
+  return await setRefereshToken(userName);
+}
 
-async function updateUser(user) {}
+async function updateAdmin(admin) {
+  try {
+    const { userName } = admin;
+    //console.log(phone);
+    await prisma.Admin.update({
+      where: { userName: userName },
+      data: {
+        firstName: admin.firstName,
+        lastName: admin.lastName,
+        password: admin.password,
+        refreshToken: admin.refreshToken,
+        permissions: admin.permissions,
+        isAllowedtoResetPassword: admin.isAllowedtoResetPassword,
+      },
+    });
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
 
-function deleteUser(userName) {}
+function deleteAdmin(userName) {}
 
 module.exports = { signup };
