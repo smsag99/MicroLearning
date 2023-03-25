da9328b7434653a800c5b765d6e83452121c2901
aryan <aryanarian00@gmail.com>
2023-03-26 03:41:03 +0430

    the  first test of new database has been passed successfully, only relations remain

---
+++ src/prisma/scripts.ts
@@ -0,0 +1,10 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+console.log("kldsdk")
+
+async function main() {
+    const user = await prisma.test.findMany()
+    console.log (user)
+}
+main()
+    
