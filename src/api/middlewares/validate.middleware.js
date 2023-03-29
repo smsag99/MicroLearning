e70c6963a5ba4647b32a9363fc7eb0af89c27c65
aryan <aryanarian00@gmail.com>
2023-03-30 04:08:39 +0430

    the extra and not used folder 'routes' out of API folder deleted

--- src/api/middlewares/validate.middleware.js
+++ src/api/middlewares/validate.middleware.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 
 const pick = (object, keys) => keys.reduce((obj, key) => {
   if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+    // eslint-disable-next-line no-param-reassign
     obj[key] = object[key];
   }
   return obj;
