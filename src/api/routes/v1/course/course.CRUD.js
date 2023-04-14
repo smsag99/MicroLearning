dbe6411662e1a9f7d954d6d9f15b3470b0bfb38f
smsag99 <smsag99@gmail.com>
2023-04-14 21:55:28 +0330

    set routes for course

--- src/api/routes/v1/course/course.CRUD.js
+++ src/api/routes/v1/course/course.CRUD.js
@@ -4,11 +4,13 @@ const { isAuth } = require("../../../middlewares/isAuth.middleware");
 const { isCan } = require("../../../middlewares/isCan.middleware");
 const { fetchAdmin } = require("../../../middlewares/fetchAdmin.middleware");
 const validate = require("../../../middlewares/validate.middleware");
+const crudCourseValidationSchema = require("../../../../validation/validation.course.services");
 const { ApiError } = require("../../../middlewares/errorHandling.middleware");
 const courseServices = require("../../../../services/course/Course.services");
-//get course
+
+//get all courses
 router.get(
-  "/",
+  "/getAllCourses",
   isAuth,
   fetchAdmin,
   isCan("read", "Course"),
@@ -20,22 +22,35 @@ router.get(
     }
   }
 );
+//get by id
 
+router.get(
+  "/:id",
+  validate(crudCourseValidationSchema.read),
+  isAuth,
+  fetchAdmin,
+  isCan("read", "Course"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const resault = await getCourseByID(id);
+      res.send(resault);
+    } catch (error) {
+      return next(new ApiError(error.statusCode, error.message));
+    }
+  }
+);
 //create course
 router.post(
   "/",
+  validate(crudCourseValidationSchema.create),
   isAuth,
   fetchAdmin,
   isCan("create", "Course"),
   async (req, res, next) => {
     try {
-      const { name, teacherId, title, description } = req.body;
-      await courseServices.createEmptyCourse(
-        name,
-        teacherId,
-        title,
-        description
-      );
+      const { teacherId, title, description } = req.body;
+      await courseServices.createEmptyCourse(teacherId, title, description);
       res.send();
     } catch (error) {
       return next(new ApiError(error.statusCode, error.message));
@@ -44,12 +59,29 @@ router.post(
 );
 //update course
 router.put(
-  "/",
+  "/:id",
+  validate(crudCourseValidationSchema.update),
   isAuth,
   fetchAdmin,
   isCan("update", "Course"),
   async (req, res, next) => {
     try {
+      req.body.id = req.params.id;
+      courseServices.updateCourse(req.body);
+    } catch (error) {
+      return next(new ApiError(error.statusCode, error.message));
+    }
+  }
+);
+router.put(
+  "/lock/:id",
+  validate(crudCourseValidationSchema.lock),
+  isAuth,
+  fetchAdmin,
+  isCan("update", "Course"),
+  async (req, res, next) => {
+    try {
+      courseServices.lockStatus(req.params.id, req.body.lockStatus);
     } catch (error) {
       return next(new ApiError(error.statusCode, error.message));
     }
