85d4852dc2b3346002fb2b0d659bcb52365d896b
smsag99 <smsag99@gmail.com>
2023-04-19 19:33:24 +0330

    the add student services has been created and tested successfully

--- src/services/course/StudentsOnClass Services.js
+++ src/services/course/StudentsOnClass Services.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 require("dotenv").config();
 
 //only admin can makes class
-async function newStudent(
+async function addStudent(
   student,
   classID,
   mark,
@@ -15,16 +15,18 @@ async function newStudent(
   endTime
 ) {
   try {
-    console.log (await prisma.studentsOnClass.create({
-      data: {
-        student: { connect: { id: student } },
-        class: { connect: { id: classID } },
-        mark: mark,
-        progress: progress,
-        startTime: startTime,
-        endTime: endTime,
-      },
-    }));
+    console.log(
+      await prisma.studentsOnClass.create({
+        data: {
+          student: { connect: { id: student } },
+          class: { connect: { id: classID } },
+          mark: mark,
+          progress: progress,
+          startTime: startTime,
+          endTime: endTime,
+        },
+      })
+    );
   } catch (error) {
     throw new ApiError(error.statusCode, error.message);
   }
@@ -33,15 +35,17 @@ async function updateStudent(updatedStudent) {
   try {
     // eslint-disable-next-line no-param-reassign
     const id = updatedStudent.id;
-    await prisma.studentsOnClass.update({
-      where: { id: id },
-      data: updatedStudent,
-    });
+    console.log(
+      await prisma.studentsOnClass.update({
+        where: { id: id },
+        data: updatedStudent,
+      })
+    );
   } catch (error) {
     throw new ApiError(error.statusCode, error.message);
   }
 }
-async function getAllStudentsOnClass() {
+async function getAllStudentsOnClass(id) {
   try {
     const StudentRecords = await prisma.studentsOnClass.findMany({
       where: { id: id },
@@ -54,23 +58,21 @@ async function getAllStudentsOnClass() {
 }
 async function getStudentByID(id) {
   try {
-    await prisma.studentsOnClass.findUnique({
-      where: {
-        id: id,
-      },
-    });
+    console.log(
+      await prisma.studentsOnClass.findUnique({
+        where: {
+          id: id,
+        },
+      })
+    );
   } catch (error) {
     throw new ApiError(500, "database error while findUnique");
   }
 }
-async function test() {
-  classes = await prisma.class.findMany();
-  students = await prisma.user.findMany();
-  console.log(classes);
-  console.log(students);
-}
-//test();
 
-newStudent(1, 1, 0, 0, new Date("2000-01-01"), new Date("2020-01-01"));
-
-module.exports = {};
+module.exports = {
+  addStudent,
+  updateStudent,
+  getAllStudentsOnClass,
+  getStudentByID,
+};
