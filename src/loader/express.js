03a4b1039bf87d4eb11e921947a9b55de07fb5aa
smsag99 <smsag99@gmail.com>
2023-07-26 14:27:47 +0330

    adding cors

--- src/loader/express.js
+++ src/loader/express.js
@@ -2,12 +2,18 @@ const express = require("express");
 const routes = require("../api/routes");
 require("dotenv").config({ path: "../.env" });
 const httpstatus = require("http-status-codes");
+const cors = require("cors");
 const {
   ApiError,
   errorHandler,
 } = require("../api/middlewares/errorHandling.middleware");
 
 const expressLoader = async (app) => {
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
   app.use(express.json());
   app.use("/api", routes());
 
