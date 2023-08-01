8b99bd1647be92e4484732235b13f18aa3c79ec3
smsag99 <smsag99@gmail.com>
2023-08-01 15:18:51 +0330

    chnage engine in prisma for solving segmentation fault

--- src/prisma/scripts.ts
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
-//console.log("kldsdk")
-
-async function main() {
-    const user = await prisma.test.findMany()
-    //console.log (user)
-}
-main()
