6b740ae2405a52a71584600a11aa77e4ce38a932
smsag99 <smsag99@gmail.com>
2023-08-30 17:06:13 +0330

    change -1

--- src/services/file-upload.js
+++ src/services/file-upload.js
@@ -11,22 +11,26 @@ module.exports = (req, directory) => {
     return new Promise((resolve, reject) => {
       const form = formidable({ multiples: false });
       form.parse(req, (err, fields, file) => {
-        if (err || !file.media){
+        if (err || !file.media) {
           reject(err || new Error("media is not provided."));
           return;
         }
-          
-        const extention = file.media.originalFilename.split('.')[-1];
+
+        const extention = file.media.originalFilename.split(".");
         const newPath = path.join(
           process.cwd(),
           "src/",
           directory,
-          uuid.v4() + "_" + extention
+          uuid.v4() + "_" + extention[extention.length - 1]
         );
-        const pathToFile = newPath.split(path.sep)[-1];
+        const pathToFile = newPath.split(path.sep);
         fs.createReadStream(file.media.filepath)
           .pipe(fs.createWriteStream(newPath))
-          .on("finish", () => resolve("http://91.107.160.88:3000/"+pathToFile))
+          .on("finish", () =>
+            resolve(
+              "http://91.107.160.88:3000/" + pathToFile[pathToFile.length - 1]
+            )
+          )
           .on("error", (error) => reject(error));
       });
     });
