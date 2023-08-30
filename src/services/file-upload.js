6339864c18e35e01f6b8439b7916a8dd25977f08
smsag99 <smsag99@gmail.com>
2023-08-30 16:59:34 +0330

    adding static address for downloading files

--- src/services/file-upload.js
+++ src/services/file-upload.js
@@ -11,19 +11,22 @@ module.exports = (req, directory) => {
     return new Promise((resolve, reject) => {
       const form = formidable({ multiples: false });
       form.parse(req, (err, fields, file) => {
-        if (err || !file.media)
+        if (err || !file.media){
           reject(err || new Error("media is not provided."));
-
+          return;
+        }
+          
+        const extention = file.media.originalFilename.split('.')[-1];
         const newPath = path.join(
           process.cwd(),
           "src/",
           directory,
-          uuid.v4() + "_" + file.media.originalFilename
+          uuid.v4() + "_" + extention
         );
-
+        const pathToFile = newPath.split(path.sep)[-1];
         fs.createReadStream(file.media.filepath)
           .pipe(fs.createWriteStream(newPath))
-          .on("finish", () => resolve(newPath.split("\\")))
+          .on("finish", () => resolve("http://91.107.160.88:3000/"+pathToFile))
           .on("error", (error) => reject(error));
       });
     });
